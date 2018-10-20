45f1a82031e3dd124b2cf200ef0d006abfb4fa12
roshan172 <pandeyroshan556@gmail.com>
2018-10-20 19:52:54 +0530

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,6 +1,6 @@
 alert("ACCESS DENIED");
 document.body.innerHTML=`
 	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-	<h1 style="font-size: 550%; color: #1ed2f4; margin-left: 2%; letter-spacing: 20px; text-align: center;">WARNING</h1>
+	<h1 style="font-size: 550%; color: #1ed2f4; margin-left: 2%; letter-spacing: 20px; text-align: center;">~WARNING~</h1>
 	<h1 style="font-size: 400%; color: #35356c; margin-left: 2%; letter-spacing: 10px; text-align: center;">!!! This site is blocked due to some reasons !!!</h1><br><br>
 `
